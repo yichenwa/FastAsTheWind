dc9a8918579f2538521f433276e3763bd172b451
al2100 <zhao.sam92@gmail.com>
2017-09-20 10:27:00 -0400

    First

---
+++ Assets/Scripts/Turns.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Turns{
+    //A turn here is just a attack.
+    //Name of attacker.
+    //The attacker object, target object.
+    public string attackerName;
+    public GameObject attackerObject;
+    public GameObject targetObject;
+   
+
+}
