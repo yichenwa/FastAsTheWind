dab4e929505d0717460cfe443122732e1edce71e
Ehab Mahran <ehabmahr@buffalo.edu>
2017-09-21 22:56:38 -0400

    Rearranged scene indices

--- Assets/Globals/SceneIndexes.cs
+++ Assets/Globals/SceneIndexes.cs
@@ -6,8 +6,8 @@ public static class SceneIndexes {
 
     public static int MainMenu() { return 0; }
     public static int WorldMap() { return 1; }
-    public static int IslandVisitation() { return 2; }
-    public static int Combat() { return 3; }
+    public static int Combat() { return 2; }
+    public static int IslandVisitation() { return 3; }
 
     // Update is called once per frame
 }
