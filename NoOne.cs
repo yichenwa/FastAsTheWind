0700181ead752b7ad677dbb5f68ba253a70ce2b9
yichenwa <yichenwa@buffalo.edu>
2017-11-01 11:18:26 -0400

    Code part
    Add code to every weapon, (if you have more than 2).

---
+++ NoOne.cs
@@ -0,0 +1,41 @@
+﻿//by yichen Oct 26
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoOne : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Active ();
+		Disappear ();
+
+	}
+	public void Active(){
+		if(Input.GetButtonDown("1"))
+		{ 
+
+			gameObject.SetActive(true);
+
+
+		} 
+
+	}
+	public void Disappear(){
+		if(Input.GetButtonDown("2"))
+		{ 
+
+			Destroy(gameObject);
+
+
+		} 
+	}
+
+}
---
+++ NoTwo.cs
@@ -0,0 +1,37 @@
+﻿//By yichen Oct 26
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoTwo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Active ();
+		Disappear ();
+	}
+	public void Active(){
+		if(Input.GetButtonDown("2"))
+		{ 
+
+			gameObject.SetActive(true);
+
+
+		} 
+
+	}
+	public void Disappear(){
+		if(Input.GetButtonDown("1"))
+		{ 
+
+			gameObject.SetActive(false);
+
+
+		} 
+	}
+}
